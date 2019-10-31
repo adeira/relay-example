0ff29fa05048f25b2b5e8f7c86152e34bd00ba77
Trond Bergquist <trond_bergquist@hotmail.com>
2019-10-31 08:24:11 +0100

    relay-example: Change x-client header to adeira
    adeira-source-id: 23711f6e76c787893611e02e888b497695b7a94a

--- src/createRelayEnvironment.js
+++ src/createRelayEnvironment.js
@@ -10,7 +10,7 @@ import {
 export default function createRelayEnvironment(initialData: ?RecordMap): Environment {
   const resource = 'https://graphql.kiwi.com';
   const fetchFn = createNetworkFetcher(resource, {
-    'X-Client': 'https://github.com/kiwicom/relay-example',
+    'X-Client': 'https://github.com/adeira/relay-example',
   });
 
   return createEnvironment({
