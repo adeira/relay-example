d511251405238a7b7534b4c5dbf78c788953a87b
Jaroslav Kubicek <jaroslav.kubicek@gmail.com>
2019-12-13 17:00:02 +0100

    Remove "scripts" folder to unblock shipit from universe

--- scripts/jest/custom-transformer.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-const path = require('path');
-const babelJest = require('babel-jest');
-
-module.exports = babelJest.createTransformer(
-  // $FlowAllowDynamicImport
-  require(path.join(__dirname, '..', '..', '.babelrc.js')),
-);
