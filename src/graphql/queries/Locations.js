02983079d886464f9e174beb3b270bd700563830
Trond Bergquist <trond_bergquist@hotmail.com>
2020-02-10 19:20:06 +0100

    Use own implementation of connectionFromArray
    Current implementation in graphql-js is broken.
    This the the solution we used in graphql.kiwi.com to patch it
    closes #406
    
    adeira-source-id: 2129ecee894b8374ac91dae7af839f3f8f919fe3

--- src/graphql/queries/Locations.js
+++ src/graphql/queries/Locations.js
@@ -1,24 +1,50 @@
 // @flow
 
-import { connectionArgs, connectionFromArray } from 'graphql-relay';
+import {
+  connectionArgs,
+  cursorToOffset,
+  connectionFromArray as relayConnectionFromArray,
+  offsetToCursor,
+  type Connection,
+  type ConnectionArguments,
+} from 'graphql-relay';
 
 import locations from '../datasets/locations.json';
 import LocationConnection from '../types/LocationConnection';
 
-type Args = {
-  +first?: number,
-  +last?: number,
-  +after?: string,
-  +number?: string,
-  ...
-};
+// There is open discusion how to handle bidirectional pagination:
+// https://github.com/graphql/graphql-relay-js/issues/58
+//
+// Until Relay devs decide, let's try our implementation
+function connectionFromArray<T>(
+  // $FlowFixMe: https://github.com/graphql/graphql-relay-js/pull/228
+  data: $ReadOnlyArray<T>,
+  args: ConnectionArguments,
+): $Exact<Connection<T>> {
+  const { edges, pageInfo } = relayConnectionFromArray(data, args);
+  const firstCursor = offsetToCursor(0);
+  const lastCursor = offsetToCursor(data.length - 1);
+  const lowerBound = cursorToOffset(pageInfo.startCursor ?? firstCursor);
+  const upperBound = cursorToOffset(pageInfo.endCursor ?? lastCursor);
+
+  // there are some data "before" current slice
+  if (lowerBound > 0) {
+    pageInfo.hasPreviousPage = true;
+  }
+  // there are some data "after" current slice
+  if (upperBound < data.length - 1) {
+    pageInfo.hasNextPage = true;
+  }
+
+  return { edges, pageInfo };
+}
 
 export default {
   type: LocationConnection,
   args: {
     ...connectionArgs,
   },
-  resolve: (_: mixed, args: Args) => {
+  resolve: (_: mixed, args: { ...ConnectionArguments, ... }) => {
     // $FlowExpectedError: graphql-relay is not using explicit inexact object
     return connectionFromArray<Location>(locations.locations, args);
   },
