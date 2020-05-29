5e45d017e507ad6a9e9c1a7d65b127a3ba9145bf
Trond Bergquist <trond_bergquist@hotmail.com>
2020-05-29 17:11:52 +0200

    relay-example: Add test for RangeAdd mutation
    adeira-source-id: fd2c6c6a6adb5639c2e2b7287037965788fa1e47

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
 // @flow strict
 
+const path = require('path');
+
 // This configuration is merged with the project configuration defined
 // in this monorepo root.
 module.exports = {
   rootDir: __dirname,
   testMatch: ['<rootDir>/**/__tests__/**/?(*.)+(spec|test).js?(x)'], // @x-shipit-enable
-  // @x-shipit-disable: transform: { '^.+\\.js$': '<rootDir>/scripts/jest/custom-transformer.js' },
+  transform: { '^.+\\.js$': '<rootDir>/scripts/jest/custom-transformer.js' }, // @x-shipit-disable,
+  setupFilesAfterEnv: [path.join(__dirname, 'scripts', 'jest', 'setupTests.js')],
 };
---
+++ src/mutations/RangeAdd/__tests__/RangeAdd.test.js
@@ -0,0 +1,92 @@
+// @flow
+
+import * as React from 'react';
+import { createMockEnvironment, MockPayloadGenerator } from 'relay-test-utils';
+import { render, screen, fireEvent, waitFor, act } from '@testing-library/react';
+import { RelayEnvironmentProvider } from '@adeira/relay';
+
+import RangeAdd from '../RangeAdd';
+
+let environment;
+
+beforeEach(() => {
+  window.scrollTo = jest.fn();
+  environment = createMockEnvironment();
+});
+
+const edges = [
+  {
+    node: {
+      id: 'loc:1',
+      name: 'Oslo',
+      type: 'CITY',
+    },
+  },
+  {
+    node: {
+      id: 'loc:2',
+      name: 'Barcelona',
+      type: 'CITY',
+    },
+  },
+];
+
+const TestRenderer = () => (
+  <RelayEnvironmentProvider environment={environment}>
+    <RangeAdd />
+  </RelayEnvironmentProvider>
+);
+
+it('shows city names in the list', () => {
+  render(<TestRenderer />);
+  environment.mock.resolveMostRecentOperation(operation =>
+    MockPayloadGenerator.generate(operation, {
+      LocationConnection: () => ({
+        edges,
+      }),
+    }),
+  );
+
+  expect(screen.getByText(/name: barcelona/i)).toBeInTheDocument();
+  expect(screen.getByText(/name: oslo/i)).toBeInTheDocument();
+});
+
+it('adds a location to the connection', async () => {
+  render(<TestRenderer />);
+  environment.mock.resolveMostRecentOperation(operation =>
+    MockPayloadGenerator.generate(operation, {
+      LocationConnection: () => ({
+        edges,
+      }),
+    }),
+  );
+  expect(screen.getByText(/name: barcelona/i)).toBeInTheDocument();
+  expect(screen.getByText(/name: oslo/i)).toBeInTheDocument();
+  expect(screen.queryByText(/name: prague/i)).not.toBeInTheDocument();
+  const idInput = screen.getByPlaceholderText('locationId');
+  const nameInput = screen.getByPlaceholderText('name');
+
+  fireEvent.change(idInput, { target: { value: 'OCD' } });
+  fireEvent.change(nameInput, { target: { value: 'Prague' } });
+  fireEvent.click(screen.getByRole('button', { name: /open drop/i }));
+  fireEvent.click(screen.getByRole('menuitem', { name: /city/i }));
+  fireEvent.click(screen.getByRole('button', { name: /submit/i }));
+
+  const operation = await waitFor(() => environment.mock.getMostRecentOperation());
+
+  act(() => {
+    environment.mock.resolve(
+      operation,
+      MockPayloadGenerator.generate(operation, {
+        Location: () => ({
+          id: 'loc: 3',
+          name: 'Prague',
+          type: 'CITY',
+        }),
+      }),
+    );
+  });
+  expect(screen.getByText(/name: barcelona/i)).toBeInTheDocument();
+  expect(screen.getByText(/name: oslo/i)).toBeInTheDocument();
+  expect(screen.getByText(/name: prague/i)).toBeInTheDocument();
+});
