41813f8152a79d9b9881ae9d1035c7c5e30c3600
Martin Zlámal <mrtnzlml@gmail.com>
2020-07-15 08:03:33 +0200

    Example: cleanup test files
    The example project is failing for a long time (see: https://github.com/adeira/relay-example/runs/868411509) so this is my first iteration in an attempt to fix it.
    
    adeira-source-id: 0fbf199891edb2d6e462b4ed1bb931cc26bbc102

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,6 @@ const path = require('path');
 module.exports = {
   rootDir: __dirname,
   testMatch: ['<rootDir>/**/__tests__/**/?(*.)+(spec|test).js?(x)'], // @x-shipit-enable
-  transform: { '^.+\\.js$': '<rootDir>/scripts/jest/custom-transformer.js' }, // @x-shipit-disable,
+  // @x-shipit-disable: transform: { '^.+\\.js$': '<rootDir>/scripts/jest/custom-transformer.js' },
   setupFilesAfterEnv: [path.join(__dirname, 'scripts', 'jest', 'setupTests.js')],
 };
