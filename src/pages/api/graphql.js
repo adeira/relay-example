95868f728487877e9e4f996242a4c559c7418f54
Trond Bergquist <trond_bergquist@hotmail.com>
2020-08-29 16:06:11 +0200

    Enable cors on gql-endpoint
    adeira-source-id: 3927c499dcde5fa972da2129dd2f923c10c8cd58

--- src/pages/api/graphql.js
+++ src/pages/api/graphql.js
@@ -1,15 +1,36 @@
 // @flow
 
 import { ApolloServer } from 'apollo-server-micro';
+import Cors from 'cors';
+import { IncomingMessage, ServerResponse } from 'http';
 
 import schema from '../../graphql/Schema';
 
 const apolloServer = new ApolloServer({ schema, introspection: true, playground: true });
-
+function initMiddleware(middleware) {
+  return (req, res) =>
+    new Promise((resolve, reject) => {
+      middleware(req, res, (result) => {
+        if (result instanceof Error) {
+          return reject(result);
+        }
+        return resolve(result);
+      });
+    });
+}
+const cors = initMiddleware(
+  Cors({
+    methods: ['GET', 'POST', 'OPTIONS'],
+  }),
+);
 export const config = {
   api: {
     bodyParser: false,
   },
 };
 
-export default (apolloServer.createHandler({ path: '/api/graphql' }): any);
+export default async function graphql(req: IncomingMessage, res: ServerResponse): Promise<empty> {
+  await cors(req, res);
+
+  return apolloServer.createHandler({ path: '/api/graphql' })(req, res);
+}
