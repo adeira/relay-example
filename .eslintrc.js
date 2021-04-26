0909a3b4c29d9f37dcc05f63f8348aca88d7e122
Martin Zlámal <mrtnzlml@gmail.com>
2021-04-26 09:57:41 -0500

    Cleanup global `.eslintrc.js`
    The purpose of this change is to remove as many things from the global config as possible and move them closer to the actual projects. This plays better with a monorepo approach where every project can have relative freedom to do what it needs/wants to do.
    
    adeira-source-id: a746661e3f07ec1b65beacc17b08a030dc2a1629

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,31 @@
 // @flow strict
 
+/* eslint-disable no-unused-vars */
+const OFF = 0;
+const WARN = 1;
+const ERROR = 2;
+/* eslint-enable no-unused-vars */
+
 module.exports = {
   env: {
     browser: true,
+  },
+  rules: {
+    'import/no-extraneous-dependencies': [
+      ERROR,
+      {
+        devDependencies: [
+          '**/*.spec.js',
+          '**/*.stories.js',
+          '**/*.test.js',
+          '**/.storybook/**',
+          '**/__flowtests__/**',
+          '**/__tests__/**',
+          '**/metro.config.js',
+          '**/scripts/**',
+          '**/webpack.config.js',
+        ],
+      },
+    ],
   },
 };
