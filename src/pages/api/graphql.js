ca070f1c58e492fad0210770a6a6d7c7302cbf19
Martin Zlámal <mrtnzlml@gmail.com>
2021-08-02 10:55:30 -0500

    Relay example: update GraphQL API code
    It seems like there were some breaking changes in `apollo-server-micro`
    and we didn't catch them. This change fixes it so that the API works
    again.
    
    adeira-source-id: 9370671689fdbf2e3ed619b35acb75a4bbfbedce

--- src/pages/api/graphql.js
+++ src/pages/api/graphql.js
@@ -6,7 +6,6 @@ import { IncomingMessage, ServerResponse } from 'http';
 
 import schema from '../../graphql/Schema';
 
-const apolloServer = new ApolloServer({ schema, introspection: true, playground: true });
 function initMiddleware(middleware) {
   return (req, res) =>
     new Promise((resolve, reject) => {
@@ -18,19 +17,31 @@ function initMiddleware(middleware) {
       });
     });
 }
+
+// https://github.com/vercel/next.js/blob/26981aa54c769734a8dc31316fb660c446fe53b9/examples/api-routes-cors/pages/api/cors.js
 const cors = initMiddleware(
   Cors({
     methods: ['GET', 'POST', 'OPTIONS'],
   }),
 );
+
+// https://nextjs.org/docs/api-routes/api-middlewares
 export const config = {
   api: {
     bodyParser: false,
   },
 };
 
-export default async function graphql(req: IncomingMessage, res: ServerResponse): Promise<empty> {
-  await cors(req, res);
+const apolloServer = new ApolloServer({ schema, introspection: true, playground: true });
+const startServer = apolloServer.start();
 
-  return apolloServer.createHandler({ path: '/api/graphql' })(req, res);
+export default async function graphql(
+  req: IncomingMessage,
+  res: ServerResponse,
+): Promise<$FlowFixMe> {
+  await cors(req, res);
+  await startServer;
+  await apolloServer.createHandler({
+    path: '/api/graphql',
+  })(req, res);
 }
