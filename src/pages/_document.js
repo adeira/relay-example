f2939b979f159e5467ce5360ab3b1b4d69ec2970
Martin Zlámal <mrtnzlml@gmail.com>
2021-08-06 07:55:14 -0500

    Relay example: remove custom font
    We are not using it anymore (SX Design takes care of it).
    It was removed here: e6340e9143e9d1ecd992b2204ab40d7e36bb8fc3
    
    adeira-source-id: ec0c64ea94c19bbd23edde14240295c0425601d1

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -23,10 +23,6 @@ export default class MyDocument extends Document {
     return (
       <html lang="en-US">
         <Head>
-          <link
-            href="https://fonts.googleapis.com/css?family=Roboto:400,400i,500,500i,700&display=optional"
-            rel="stylesheet"
-          />
           <link rel="icon" href="https://adeira.dev/img/favicon.ico" />
           <style dangerouslySetInnerHTML={{ __html: mediaStyles }} />
         </Head>
