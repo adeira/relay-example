b2c7c629a1994aa2b97772ea53a53458b1b0b4bc
Trond Bergquist <trond_bergquist@hotmail.com>
2021-09-29 16:04:31 +0200

    example-relay: add next bundle analyzer
    adeira-source-id: 370d60b7081b5d86d1014f3ab5e78db09a2f22bf

--- next.config.js
+++ next.config.js
@@ -3,35 +3,40 @@
 const path = require('path');
 const withTranspileModules = require('next-transpile-modules');
 const withCustomBabelConfigFile = require('next-plugin-custom-babel-config');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 // @x-shipit-disable: const getTranspileWorkspaces = require('./scripts/getTranspileWorkspaces');
 
 // @x-shipit-disable: const transpileWorkspaces = getTranspileWorkspaces();
 const transpileWorkspaces = []; // @x-shipit-enable
 
-module.exports = (withCustomBabelConfigFile(
-  withTranspileModules(transpileWorkspaces)({
-    images: {
-      domains: ['images.kiwi.com'],
-    },
-    babelConfigFile: path.join(
-      __dirname,
-      'babel.config.js', // @x-shipit-enable
-      // @x-shipit-disable: '.babelrc.js',
-    ),
-    webpack: (config) => {
-      config.module.rules.push({
-        type: 'javascript/auto',
-        test: /\.mjs$/,
-      });
+module.exports = (withBundleAnalyzer(
+  withCustomBabelConfigFile(
+    withTranspileModules(transpileWorkspaces)({
+      images: {
+        domains: ['images.kiwi.com'],
+      },
+      babelConfigFile: path.join(
+        __dirname,
+        'babel.config.js', // @x-shipit-enable
+        // @x-shipit-disable: '.babelrc.js',
+      ),
+      webpack: (config) => {
+        config.module.rules.push({
+          type: 'javascript/auto',
+          test: /\.mjs$/,
+        });
 
-      return config;
-    },
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    experimental: {
-      esmExternals: 'loose',
-    },
-  }),
-) /*: any */);
+        return config;
+      },
+      eslint: {
+        ignoreDuringBuilds: true,
+      },
+      experimental: {
+        esmExternals: 'loose',
+      },
+    }),
+  ),
+) /*: any  */);
