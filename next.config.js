ef0a204eec3cb2bc0203a822f54e63a32c8d1892
Trond Bergquist <trond_bergquist@hotmail.com>
2021-10-01 10:09:10 +0200

    example-relay: use next-compose-plugins
    adeira-source-id: 540da4f8c458544b380244a4077793ed9d8a8eba

--- next.config.js
+++ next.config.js
@@ -6,37 +6,37 @@ const withCustomBabelConfigFile = require('next-plugin-custom-babel-config');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
+const withPlugins = require('next-compose-plugins');
 
 // @x-shipit-disable: const getTranspileWorkspaces = require('./scripts/getTranspileWorkspaces');
 
 // @x-shipit-disable: const transpileWorkspaces = getTranspileWorkspaces();
 const transpileWorkspaces = []; // @x-shipit-enable
 
-module.exports = (withBundleAnalyzer(
-  withCustomBabelConfigFile(
-    withTranspileModules(transpileWorkspaces)({
-      images: {
-        domains: ['images.kiwi.com'],
-      },
-      babelConfigFile: path.join(
-        __dirname,
-        'babel.config.js', // @x-shipit-enable
-        // @x-shipit-disable: '.babelrc.js',
-      ),
-      webpack: (config) => {
-        config.module.rules.push({
-          type: 'javascript/auto',
-          test: /\.mjs$/,
-        });
+module.exports = (withPlugins(
+  [withBundleAnalyzer, withCustomBabelConfigFile, withTranspileModules(transpileWorkspaces)],
+  {
+    images: {
+      domains: ['images.kiwi.com'],
+    },
+    babelConfigFile: path.join(
+      __dirname,
+      'babel.config.js', // @x-shipit-enable
+      // @x-shipit-disable: '.babelrc.js',
+    ),
+    webpack: (config) => {
+      config.module.rules.push({
+        type: 'javascript/auto',
+        test: /\.mjs$/,
+      });
 
-        return config;
-      },
-      eslint: {
-        ignoreDuringBuilds: true,
-      },
-      experimental: {
-        esmExternals: 'loose',
-      },
-    }),
-  ),
+      return config;
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    experimental: {
+      esmExternals: 'loose',
+    },
+  },
 ) /*: any  */);
